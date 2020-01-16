16bd49da8d1e2752a2624e3238dbd10d57a9cf0a
Hamza Ed-doubi <hed-douibi@uoc.edu>
2020-01-16 17:40:10 +0100

    Update variable names

--- dev/main.js
+++ dev/main.js
@@ -1,5 +1,5 @@
 
 import {renderXatkitWidget} from "../src/utils/renderer";
 
-renderXatkitWidget("xatkit-chat",window.title, window.subtitle, window.toggleChat, window.senderPlaceHolder);
+renderXatkitWidget("xatkit-chat",window.xatkit_title, window.xatkit_subtitle, window.xatkit_start_minimized, window.xatkit_place_holder);
 
