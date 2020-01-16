b5456e08e7d2593c97e87ba77e4cdb8997c78d19
Hamza Ed-doubi <hed-douibi@uoc.edu>
2020-01-16 10:36:05 +0100

    export a script to include directly in html which uses browser variables

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ import {
   renderXatkitWidget,
     renderDefaultXatkitWidget
 } from "./src/utils/renderer";
-
-export default  XatkitWidget;
+export  default XatkitWidget;
 export {
   addUserMessage,
   addResponseMessage,
