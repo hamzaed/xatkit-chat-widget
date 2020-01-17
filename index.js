5bbeacf1d263b934bcea7d63f1ca8811ca395cc5
Hamza Ed-doubi <hed-douibi@uoc.edu>
2020-01-17 15:33:52 +0100

    Reoganize exports

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 import XatkitWidget from './src/components/XatkitWidget';
-import {
-  addUserMessage,
-  addResponseMessage,
-  addLinkSnippet,
-  renderCustomComponent,
-  toggleWidget,
-  toggleInputDisabled,
-  toggleMsgLoader,
-  dropMessages,
-  isWidgetOpened,
-  setQuickButtons
-} from './src/store/dispatcher';
+
 import {
   renderXatkitWidget,
-    renderDefaultXatkitWidget
+  renderDefaultXatkitWidget
 } from "./src/utils/renderer";
+
 export  default XatkitWidget;
+
 export {
-  addUserMessage,
-  addResponseMessage,
-  addLinkSnippet,
-  renderCustomComponent,
-  toggleWidget,
-  toggleInputDisabled,
-  toggleMsgLoader,
-  dropMessages,
-  isWidgetOpened,
-  setQuickButtons,
-    renderDefaultXatkitWidget,
-    renderXatkitWidget
+  renderDefaultXatkitWidget,
+  renderXatkitWidget
 };
+
