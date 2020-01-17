61f8e20a4213effa00c97a2ee427455cdf691113
Hamza Ed-doubi <hed-douibi@uoc.edu>
2020-01-17 15:41:42 +0100

    Update main.js

--- dev/main.js
+++ dev/main.js
@@ -1,5 +1,6 @@
 
-import {renderXatkitWidget} from "../src/utils/renderer";
+import {renderDefaultXatkitWidget, renderXatkitWidget} from "../index";
 
-renderXatkitWidget("xatkit-chat",window.xatkit_title, window.xatkit_subtitle, window.xatkit_start_minimized, window.xatkit_place_holder);
+//renderXatkitWidget("xatkit-chat","My title", "My subtitle", false, "Write...");
+renderDefaultXatkitWidget();
 
