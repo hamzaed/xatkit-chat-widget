63a4ecc270566845b34aa96eeb8be35668dbe1a9
Hamza Ed-doubi <hed-douibi@uoc.edu>
2020-02-03 16:01:38 +0100

    Fix broken tests after fixing [#13]

--- src/components/Widget/components/Launcher/test/index.test.js
+++ src/components/Widget/components/Launcher/test/index.test.js
@@ -5,20 +5,25 @@ import Adapter from 'enzyme-adapter-react-16';
 import Launcher from '../index';
 import Badge from '../components/Badge';
 
+import openLauncher from '@assets/xatkit-avatar-negative.png';
+
 configure({ adapter: new Adapter() });
 
 describe('<Launcher />', () => {
-  const createMessageComponent = ({ toggle, chatOpened, badge = 0 }) =>
+  const createMessageComponent = ({ toggle, chatOpened, badge = 0, launcherImage }) =>
     shallow(<Launcher.WrappedComponent
       toggle={toggle}
       chatOpened={chatOpened}
       badge={badge}
+      launcherImage={launcherImage}
     />);
 
   it('should call toggle prop when clicked', () => {
     const toggle = jest.fn();
     const chatOpened = false;
-    const launcherComponent = createMessageComponent({ toggle, chatOpened });
+    const badge = 0;
+    const launcherImage = openLauncher;
+    const launcherComponent = createMessageComponent({ toggle, chatOpened ,badge,launcherImage});
     launcherComponent.find('.rcw-launcher').simulate('click');
     expect(toggle).toBeCalled();
   });
@@ -26,7 +31,9 @@ describe('<Launcher />', () => {
   it('should render the open-launcher image when chatOpened = false', () => {
     const toggle = jest.fn();
     const chatOpened = false;
-    const launcherComponent = createMessageComponent({ toggle, chatOpened });
+    const badge = 0;
+    const launcherImage = openLauncher;
+    const launcherComponent = createMessageComponent({ toggle, chatOpened, badge, launcherImage});
     expect(launcherComponent.find('.rcw-open-launcher')).toHaveLength(1);
   });
 
@@ -41,7 +48,8 @@ describe('<Launcher />', () => {
     const toggle = jest.fn();
     const chatOpened = false;
     const badge = 1;
-    const launcherComponent = createMessageComponent({ toggle, chatOpened, badge });
+    const launcherImage = openLauncher;
+    const launcherComponent = createMessageComponent({ toggle, chatOpened, badge,launcherImage });
     expect(launcherComponent.find(Badge).props().badge).toBe(1);
   })
 });
