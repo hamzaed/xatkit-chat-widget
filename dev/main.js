7a3573ec91abe5e5536d426c7016f5b05ac5f58d
Gwendal Daniel <gwendal.daniel@gmail.com>
2020-02-04 00:52:52 +0100

    removed explicit parameters in main.js

--- dev/main.js
+++ dev/main.js
@@ -1,7 +1,3 @@
 
 import {renderXatkitWidget} from "../index";
-import avatar from '@assets/xatkit-avatar.png';
-import launcherImage from '@assets/xatkit-avatar-negative.png';
-//renderXatkitWidget();
-renderXatkitWidget(undefined, 'bot','xatkit-chat','My title', 'My subtitle', true,'My placeholder',avatar,launcherImage );
-
+renderXatkitWidget();
