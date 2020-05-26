83ce353c2ef7d4be5fb851e18a5fc8529bca3530
Gwendal Daniel <gwendal.daniel@gmail.com>
2020-05-26 12:35:31 +0200

    renderXWidget defaults to the xatkit avatar when provided empty string

--- src/utils/renderer.js
+++ src/utils/renderer.js
@@ -1,8 +1,18 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import XatkitWidget from "../components/XatkitWidget";
+import xatkitAvatar from '@assets/xatkit-avatar.png';
 
 export function renderXatkitWidget(server, username,elementId,title,subtitle,startMinimized, senderPlaceholder, profileAvatar, launcherImage, buttonsPlaceholder, hostname, url, origin) {
+    /*
+     * Check if the provided profileAvatar is an empty string, in this case we display the default Xatkit logo and log
+     * and error in the console.
+     */
+    let avatar = profileAvatar;
+    if(avatar === "") {
+        console.log("Empty string provided as avatar, using the default Xatkit avatar");
+        avatar = xatkitAvatar;
+    }
     const domElement = elementId === undefined ? document.getElementById('xatkit-chat') : document.getElementById(elementId);
     ReactDOM.render(<XatkitWidget
         server={server}
@@ -11,7 +21,7 @@ export function renderXatkitWidget(server, username,elementId,title,subtitle,sta
         subtitle={subtitle}
         startMinimized={startMinimized}
         senderPlaceHolder={senderPlaceholder}
-        profileAvatar={profileAvatar}
+        profileAvatar={avatar}
         launcherImage={launcherImage}
         buttonsPlaceholder={buttonsPlaceholder}
         hostname={hostname}
