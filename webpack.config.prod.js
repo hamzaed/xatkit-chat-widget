b36a66715c3dba1e665f7a8d0665d6ffdd47f45b
Hamza <hamza.eddouibi@gmail.com>
2020-05-28 13:29:19 +0200

    Update webpack conf. to generate non-minified versions of css and js
    [#46] now we have two versions of js and css (minified and non-minified)
    [#25] we still need to generate a sourcemap because the non-minimized js file is just a version of the minimized one but with comments and white spaces

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const webpack = require('webpack');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -69,8 +68,8 @@ module.exports = [
      * the following package: sudo apt-get install libpng16-dev
      */
     new MiniCssExtractPlugin({
-      filename: 'xatkit.css',
-      chunkFileName: '[id].css'
+      filename: 'xatkit.min.css',
+      chunkFileName: '[id].min.css'
     }),
   ],
   optimization: {
@@ -141,7 +140,6 @@ module.exports = [
       ]
     },
     plugins: [
-      new CleanWebpackPlugin(['lib']),
       /**
        * Known issue for the CSS Extract Plugin in Ubuntu 16.04: You'll need to install
        * the following package: sudo apt-get install libpng16-dev
@@ -149,6 +147,73 @@ module.exports = [
       new MiniCssExtractPlugin({
         filename: 'xatkit.css',
         chunkFileName: '[id].css'
+      })
+    ],
+    optimization: {
+      minimize: false
+    }
+  },
+  {
+    entry: './index.js',
+    output: {
+      path: path.join(__dirname, '/lib'),
+      filename: 'xatkit.min.js',
+      library: 'xatkit',
+      libraryTarget: 'var'
+    },
+    resolve: {
+      extensions: ['.js']
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'), // eslint-disable-line
+                  autoprefixer({
+                    browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie <9'],
+                    flexbox: 'no-2009'
+                  })
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [path.resolve(__dirname, 'src/scss/')]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use: {
+            loader: 'url-loader'
+          }
+        }
+      ]
+    },
+    plugins: [
+      /**
+       * Known issue for the CSS Extract Plugin in Ubuntu 16.04: You'll need to install
+       * the following package: sudo apt-get install libpng16-dev
+       */
+      new MiniCssExtractPlugin({
+        filename: 'xatkit.min.css',
+        chunkFileName: '[id].min.css'
       }),
     ],
     optimization: {
@@ -165,4 +230,5 @@ module.exports = [
       ]
     }
   }
+
 ];
