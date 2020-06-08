6522948142f6e3c452a5f4aaa38de3a87630ce41
Hamza <hamza.eddouibi@gmail.com>
2020-06-08 12:06:05 +0200

    Save the current state to localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -4,13 +4,30 @@ import behavior from './reducers/behaviorReducer';
 import messages from './reducers/messagesReducer';
 import quickButtons from './reducers/quickButtonsReducer';
 
+const saveToLocaleStorage = (state) => {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('Xatkit_widget_state',serializedState)
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+
 const reducer = combineReducers({ behavior, messages, quickButtons });
 
-export default createStore(
-  reducer,
-  process.env.NODE_ENV !== 'production' ?
-    /* eslint-disable no-underscore-dangle */
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__() : ''
+const store = createStore(
+    reducer,
+    process.env.NODE_ENV !== 'production' ?
+        /* eslint-disable no-underscore-dangle */
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__() : ''
     /* eslint-enable */
-);
+)
+
+store.subscribe(()=> {
+    saveToLocaleStorage(store.getState())
+})
+
+export default store
